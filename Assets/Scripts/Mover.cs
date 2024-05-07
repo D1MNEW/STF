6747defcdad348dfb267e08f2d1422e741a889a2
D1MNEW <158606804+D1MNEW@users.noreply.github.com>
2024-05-07 18:14:42 +0300

    скелет
    анимки

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     public float _speed = 0;
+    public Animator Animator;
 
     void Start()
     {
@@ -12,37 +13,48 @@ public class Mover : MonoBehaviour
     }
     void Update()
     {
-
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.forward * _speed;
-            Vector3 rotate = transform.eulerAngles;
-            rotate.y = 0;
-            transform.rotation = Quaternion.Euler(rotate);
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += Vector3.forward * _speed;
+                Vector3 rotate = transform.eulerAngles;
+                rotate.y = 0;
+                transform.rotation = Quaternion.Euler(rotate);
+                Animator.SetBool("Go", true);
+
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += Vector3.back * _speed;
+                Vector3 rotate = transform.eulerAngles;
+                rotate.y = 180;
+                transform.rotation = Quaternion.Euler(rotate);
+                Animator.SetBool("Go", true);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += Vector3.left * _speed;
+                Vector3 rotate = transform.eulerAngles;
+                rotate.y = 270;
+                transform.rotation = Quaternion.Euler(rotate);
+                Animator.SetBool("Go", true);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += Vector3.right * _speed;
+                Vector3 rotate = transform.eulerAngles;
+                rotate.y = 90;
+                transform.rotation = Quaternion.Euler(rotate);
+                Animator.SetBool("Go", true);
+            }
         }
-
-        if (Input.GetKey(KeyCode.S))
+        else
         {
-            transform.position += Vector3.back * _speed;
-            Vector3 rotate = transform.eulerAngles;
-            rotate.y = 180;
-            transform.rotation = Quaternion.Euler(rotate);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += Vector3.left * _speed;
-            Vector3 rotate = transform.eulerAngles;
-            rotate.y = 270;
-            transform.rotation = Quaternion.Euler(rotate);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += Vector3.right * _speed;
-            Vector3 rotate = transform.eulerAngles;
-            rotate.y = 90;
-            transform.rotation = Quaternion.Euler(rotate);
+            Animator.SetBool("Go", false);
         }
     }
 
