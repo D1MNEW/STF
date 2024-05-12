678aebb997304917b76d57ec851d4113f322c15c
Tarrlay <rederevo430@gmail.com>
2024-05-12 18:38:53 +0300

    чуть поменял локу и там да
    расширил территорию, добавил деревца и ну как-то сделал чето похожее на 1 из 4 частей для боевки чет типо этого, поставил камеру на первое лицо

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,24 +8,23 @@ public class CameraRotation : MonoBehaviour
     public float minAngle;
     public float maxAngle;
 
-    public float RotationSpeed;
-    
+    public float RotationSpeed;         
+    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    
+    // Update is called once per frame
     void Update()
     {
-        var newAngleY = transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X");
-        transform.localEulerAngles = new Vector3(0, newAngleY, 0);
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
 
         var newAngleX = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y");
-        if (newAngleX > 180)
+        if (newAngleX > 180) 
             newAngleX -= 360;
-
         newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
+
         CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
     }
 }
