85616bd519f12ea9d7ff874fa8a5df35b325d248
Tarrlay <rederevo430@gmail.com>
2024-05-17 19:43:26 +0300

    2 локи(без челов)

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -8,7 +8,7 @@ public class CameraRotation : MonoBehaviour
     public float minAngle;
     public float maxAngle;
 
-    public float RotationSpeed;         
+    public float RotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +21,7 @@ public class CameraRotation : MonoBehaviour
         transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
 
         var newAngleX = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y");
-        if (newAngleX > 180) 
+        if (newAngleX > 180)
             newAngleX -= 360;
         newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
 
