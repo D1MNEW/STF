0d13750bbc9176bc1cd0aad46a3a56faa5109f51
Tarrlay <rederevo430@gmail.com>
2024-05-20 19:31:59 +0300

    полностью доделал переходы
    от начала до основной локи, сделал обучающую сцену!!! и сделал чет там еще много короче, тогда ты доделаешь оставшуюся часть, ок?

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadNextLevel()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        var newSceneIndex = currentSceneIndex + 1;
+        PlayerPrefs.SetInt("currentLevel", newSceneIndex);
+        SceneManager.LoadScene(newSceneIndex);
+    }
+
+    // Update is called once per frame
+    //public void BackToMainMenu()
+    //{
+    //    SceneManager.LoadScene(0);
+    //}
+
+    //public void NewGame()
+    //{
+    //    PlayerPrefs.SetInt("currentlevel", 1);
+    //    SceneManager.LoadScene(1);
+    //}
+    //public void LoadCurrentLevel()
+    //{
+    //    var currentLevel = PlayerPrefs.GetInt("currentLevel");
+    //    SceneManager.LoadScene(currentLevel);
+    //}
+}
---
+++ Assets/Scripts/NLVL.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NLVL : MonoBehaviour
+{
+        public void GameStartButton()
+        {
+            SceneManager.LoadScene(2);
+        }
+
+        // Update is called once per frame
+        void Update()
+    {
+
+    }
+        
+    
+}
