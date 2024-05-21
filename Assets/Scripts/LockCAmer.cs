ee41633c3f4cd4efe674c67e1afb7cbfef9676fc
Tarrlay <rederevo430@gmail.com>
2024-05-21 18:59:05 +0300

    локер курсора

---
+++ Assets/Scripts/LockCAmer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCAmer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
